aafd2f5a99e596c2b8d76c2d378854a728bc204f
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-02-16 16:22:18 +0200

    Small update for db module

--- xopay/db/__init__.py
+++ xopay/db/__init__.py
@@ -1,1 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Module, that inherit all nested models,
+so they can be for imported from db module
+"""
+
+from .backend import *
+from .enum import *
+
 __author__ = 'Kostel Serhii'
--- xopay/db/backend.py
+++ xopay/db/backend.py
@@ -1,1 +1,4 @@
+# -*- coding: utf-8 -*-
+""" Module for database backend connection """
+
 __author__ = 'Kostel Serhii'
