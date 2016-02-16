4dfc4c7d0d970493a74e6e47401b42c69a49e917
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-02-16 19:00:25 +0200

    Add favicon

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 #!venv/bin/python
+# -*- coding: utf-8 -*-
 
 from xopay import app
 
 __author__ = 'Kostel Serhii'
 
-app.run(host='0.0.0.0', port=8128, debug=True)
+app.run(host='0.0.0.0', port=7128, debug=True)
