0f49e4c4760753dd8de16b38715a1db769da493d
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-02-16 18:19:31 +0200

    Base view for main single page. Change path for static

--- xopay/__init__.py
+++ xopay/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 
@@ -6,7 +6,7 @@ from xopay.tools import json_tools
 
 __author__ = 'Kostel Serhii'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/static')
 app.config.from_object('config')
 app.json_encoder = json_tools.JSONModelEncoder
 
@@ -17,5 +17,19 @@ lm.login_view = 'login'
 db = SQLAlchemy(app)
 
 
-from xopay import mainpage
-from xopay.users import views
+@app.route('/admin/')
+@app.route('/admin/<path:path>/')
+def admin_page(path=None):
+    """
+    Return single page html for xopay admin
+    :param path: any valid url (used in frontend routing system)
+    :return: index html page
+    """
+    # TODO: enable this only in debug mode. In production use apache for this purpose
+    return app.send_static_file('index.html')
+
+
+@app.route('/')
+def index():
+    """ Redirect from root to admin page """
+    return redirect(url_for('admin_page'))
