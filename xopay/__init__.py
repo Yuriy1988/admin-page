ed506e33a7354fff5e6bad88bf1ab7070fe9081e
Serhii Kostel <serhii.kostel@digitaloutlooks.com>
2016-02-23 14:23:38 +0200

    Remove unused tools

--- xopay/__init__.py
+++ xopay/__init__.py
@@ -4,14 +4,12 @@ from flask import Flask
 from flask.ext.login import LoginManager
 
 from xopay.backend import db
-from xopay.tools import json_tools
 from config import STATIC_FOLDER
 
 __author__ = 'Kostel Serhii'
 
 app = Flask(__name__, static_folder=STATIC_FOLDER)
 app.config.from_object('config')
-app.json_encoder = json_tools.JSONModelEncoder
 
 db.init_app(app)
 
