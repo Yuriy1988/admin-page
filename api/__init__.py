f9ec7607be09fb11cf904dc68ea576e101bba094
Daniel Omelchenko <daniel.omelchenko@digitaloutlooks.com>
2016-09-06 14:17:27 +0300

    add np-cache header.

--- api/__init__.py
+++ api/__init__.py
@@ -35,6 +35,19 @@ def after_app_created(func):
     return wraps(func)
 
 
+@api_v1.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
 # Extended JSON encoder
 
 class XOPayJSONEncoder(json.JSONEncoder):
